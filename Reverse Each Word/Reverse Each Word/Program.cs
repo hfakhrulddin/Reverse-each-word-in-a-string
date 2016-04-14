a24b11c3da29617203a3001db8e0a2cfffe6d353
Hussen Fakhrulddin <hf556482@gmail.com>
2016-04-13 21:44:19 -0700

    init

---
+++ Reverse Each Word/Reverse Each Word/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Each_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the list of words");
+            string inputlist = Console.ReadLine();
+
+            string outputlist = string.Join(" ", inputlist
+                .Split(' ')
+                .Select(x => new String(x.Reverse().ToArray())));
+
+
+
+
+                Console.WriteLine(outputlist);
+                Console.Read();
+        }
+    }
+}
